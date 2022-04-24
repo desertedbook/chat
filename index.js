d3f3acce8aa605717c1258866e80550849bc550e
Rubenbarbosa <100330759+desertedbook@users.noreply.github.com>
2022-04-23 23:24:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+const http = require('http');
+const server = http.createServer(app);
+
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    //console.log('Un usuario se ha conectado.');
+
+    // socket.on('disconnect', () => {
+    //     console.log('Un usuario se ha desconectado');
+    // });
+
+    // socket.on('chat', (msg) => {
+    //     console.log('Mensaje: ' + msg)
+    // })
+
+    socket.on('chat', (msg) => {
+        io.emit('chat', msg)
+    })
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+});
+
+server.listen(3000, () => {
+    console.log('Servidor corriendo en localhost:3000')
+});
